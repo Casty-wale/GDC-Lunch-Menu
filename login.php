88c69effbb9258cb69cccd1bffac047396600ab1
Casty-wale <bo6346279@gmail.com>
2021-11-16 14:06:49 +0000

    first commit

---
+++ login.php
@@ -0,0 +1,30 @@
+<?php
+
+	include 'connect.php';
+	session_start();
+
+	if(isset($_POST['login'])){
+		$email = $_POST['email'];
+		$password = $_POST['pwd'];
+		
+        $sql = "SELECT * FROM `users` WHERE email = '$email'";
+
+        $query = $conn->query($sql); 
+
+		if($query->num_rows < 1){
+			$_SESSION['error'] = 'Wrong input';
+		}
+		else{
+			$row = $query->fetch_assoc();
+			if(password_verify($password, $row['password'])){
+				$_SESSION['users'] = $row['id'];
+			}
+			else{
+				$_SESSION['error'] = 'Wrong Password';
+			}
+		}
+
+    }
+	
+	header('location: index.php');
+?>
